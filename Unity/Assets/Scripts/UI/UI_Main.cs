50f6d5499a9515c3bbcadd1b642455c6cdd8c0cd
setsuodu <339131001@qq.com>
2022-04-17 11:24:38 +0800

    修改主界面

--- Unity/Assets/Scripts/UI/UI_Main.cs
+++ Unity/Assets/Scripts/UI/UI_Main.cs
@@ -3,28 +3,29 @@ using UnityEngine.UI;
 
 public class UI_Main : UIBase
 {
-    [SerializeField] Button m_MatchButton;
-    [SerializeField] Button m_SingleButton;
+    [SerializeField] Button m_JoinButton;
+    [SerializeField] Button m_CreateButton;
     [SerializeField] Button m_SettingsButton;
 
     void Awake()
     {
-        m_MatchButton = transform.Find("MatchBtn").GetComponent<Button>();
-        m_SingleButton = transform.Find("SingleBtn").GetComponent<Button>();
+        m_JoinButton = transform.Find("MatchBtn").GetComponent<Button>();
+        m_CreateButton = transform.Find("SingleBtn").GetComponent<Button>();
         m_SettingsButton = transform.Find("SettingsBtn").GetComponent<Button>();
 
-        m_MatchButton.onClick.AddListener(OnMatchBtnClick);
-        m_SingleButton.onClick.AddListener(OnSingleBtnClick);
+        m_JoinButton.onClick.AddListener(OnJoinBtnClick);
+        m_CreateButton.onClick.AddListener(OnCreateBtnClick);
         m_SettingsButton.onClick.AddListener(OnSettingsBtnClick);
     }
 
-    void OnMatchBtnClick()
+    void OnJoinBtnClick()
     {
-
+        //TODO: 弹出大厅列表
     }
 
-    void OnSingleBtnClick()
+    void OnCreateBtnClick()
     {
+        //TODO: 弹出创建游戏选项，人数、人机、是否公开
         var obj = new GameObject("TEMP_Local");
         var script = obj.AddComponent<TEMP_Local>();
         UIManager.Get().Push<UI_Game>();
